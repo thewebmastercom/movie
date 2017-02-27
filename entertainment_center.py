5f2b95f9b0f793e63921b99ca54eec82af4b9edf
Jonathan Griffin <jonathan@thewebmaster.com>
2017-02-27 19:25:28 +0000

    Initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,23 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life",
+                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+avatar = media.Movie("Avatar", "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+alien = media.Movie("Alien", "An alien on a human spaceship kills everyone",
+                     "https://upload.wikimedia.org/wikipedia/en/c/c3/Alien_movie_poster.jpg",
+                     "https://www.youtube.com/watch?v=bEVY_lonKf4")
+school_of_rock = media.Movie("School of Rock", "Rock singer substitutes at prestigious rock school",
+                     "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                     "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+ratatouille = media.Movie("Ratatouille", "Computer animated garbage about a mouse.",
+                     "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                     "https://www.youtube.com/watch?v=niD-jahFURU")
+hunger_games = media.Movie("Hunger Games", "A tale of deadly games in a post-apocalyptic future",
+                     "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                     "https://www.youtube.com/watch?v=4S9a5V9ODuY")
+
+movies = [toy_story, avatar, alien, school_of_rock, ratatouille, hunger_games]
